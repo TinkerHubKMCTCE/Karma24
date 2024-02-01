b6010113d061d732a3f8574892e23569088df9ef
Basith P <basithpofficial@gmail.com>
2024-02-01 15:39:03 +0530

    feat: events section

---
+++ src/views/home/components/Events/components/EventCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const EventCard = ({ title, image }) => {
+  return (
+    <div
+      className="p-8 rounded-3xl mx-auto workshop-item"
+      style={{
+        backgroundImage: `url(${image})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <h3 className="text-white font-bold font-retroTeam text-5xl">{title}</h3>
+    </div>
+  );
+};
+
+export default EventCard;
--- src/views/home/components/Hero/Hero.jsx
+++ src/views/home/components/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import karma24 from "../../../../assets/karma24.svg";
-import budha from "../../../../assets/budha.svg"
+import budha from "../../../../assets/budha.svg";
 import styles from "./Hero.module.css";
 import Countdown from "../Countdown/Countdown";
 
@@ -7,31 +7,34 @@ const Hero = () => {
   const targetDate = new Date("2024-03-01T00:00:00");
 
   return (
-    <div className="container mx-auto px-10">
-      <div id={styles.hero} className="min-h-screen flex flex-col">
-      <div className="">
-        <div className="grid grid-cols-1 md:grid-cols-2 justify-center items-center mt-6"  id={styles.title}>
-          <div className="grid place-items-center mt-12">
-            <img className="max-w-xl mx-auto w-full" src={karma24} alt="KARMA'24" />
-            <p className="mt-6 font-inversionz text-center text-3xl uppercase font-medium">
-              what goes around comes around
-            </p>
-            <button className="bg-[#09D3FF] hover:bg-[#8EFF09] text-black font-bold text-lg py-2 px-10 tracking-wide rounded-xl place-items-center mt-8 ">
-              Buy Tickets
-            </button>
+    <div className="container mx-auto pt-10 pb-20">
+      <div id={styles.hero} className="h-screen max-h-fit flex flex-col">
+        <div className="">
+          <div
+            className="grid grid-cols-1 md:grid-cols-2 justify-center items-center mt-6"
+            id={styles.title}
+          >
+            <div className="grid place-items-center mt-12">
+              <img className="max-w-xl mx-auto w-full" src={karma24} alt="KARMA'24" />
+              <p className="mt-6 font-inversionz text-center text-3xl uppercase font-medium">
+                what goes around comes around
+              </p>
+              <button className="bg-[#09D3FF] hover:bg-[#8EFF09] text-black font-bold text-lg py-2 px-10 tracking-wide rounded-xl place-items-center mt-8 ">
+                Buy Tickets
+              </button>
+            </div>
+            <div>
+              <img className="max-w-xl mx-auto mt-6 w-full" src={budha} alt="budha" />
+            </div>
           </div>
-          <div>
-            <img className="max-w-xl mx-auto mt-6 w-full" src={budha} alt="budha" />
+          <div className="grid grid-cols-4 gap-4 md:gap-10 max-w-2xl mx-auto">
+            <Countdown targetDate={targetDate} title="Days" />
+            <Countdown targetDate={targetDate} title="Hours" />
+            <Countdown targetDate={targetDate} title="Minutes" />
+            <Countdown targetDate={targetDate} title="Seconds" />
           </div>
-        </div>
-        <div className="grid grid-cols-4 gap-4 md:gap-10 max-w-2xl mx-auto">
-          <Countdown targetDate={targetDate} title="Days" />
-          <Countdown targetDate={targetDate} title="Hours" />
-          <Countdown targetDate={targetDate} title="Minutes" />
-          <Countdown targetDate={targetDate} title="Seconds" />
         </div>
       </div>
-    </div>
     </div>
   );
 };
