3f133e496ec3d1ac0412fef16f5591df5b6b5bfd
Hashimrahman <hashimrahmanrh@gmail.com>
2024-02-01 11:15:25 +0530

    feat: add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,15 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./views/home/Home";
 import Navbar from "./components/Navbar/Navbar";
 import LoginView from "./views/home/components/Ambassadors/login";
+import MainLayout from "./layouts/MainLayout";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/login", element: <LoginView /> },
+  { path: "/", element: <MainLayout />,
+  children: [
+    { path: "/", element: <Home /> },
+    { path: "/login", element: <LoginView /> },
+  ]
+ },
 ]);
 
 function App() {
