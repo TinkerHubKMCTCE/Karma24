e8127d81d699ecaea04823e89177e33a4d22bbeb
Basith P <59326665+Basith-P@users.noreply.github.com>
2024-02-10 23:51:57 +0530

    Update AmbassadorDashboard.jsx

--- src/views/AmbassadorDashboard.jsx
+++ src/views/AmbassadorDashboard.jsx
@@ -2,9 +2,9 @@ const AmbassadorDashboard = () => {
   return (
     <div className="container mx-auto pb-20 max-w-screen-xl pt-28">
       <p className="font-retroTeam text-3xl text-center text-karma-blue mb-2">
-        Your Refferal Code
+        Refferal code will be available from
       </p>
-      <p className="text-white font-bold font-orbitron text-7xl text-center">CA24023</p>
+      <p className="text-white font-bold font-orbitron text-7xl text-center">11 Feb 2024</p>
     </div>
   );
 };
