9bc8b3914222e9ad87f2820228587b9566d45ddd
Basith P <basithpofficial@gmail.com>
2024-02-13 10:53:40 +0530

    fix: ca login issue

--- src/views/home/components/Ambassadors/login.jsx
+++ src/views/home/components/Ambassadors/login.jsx
@@ -147,7 +147,6 @@ const LoginView = () => {
                     </a>
                   </div> */}
                   <button
-                    onClick={submitForm}
                     type="submit"
                     className="w-full text-black bg-karma-green hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-primary-600 hover:bg-primary-700 focus:ring-primary-800"
                   >
