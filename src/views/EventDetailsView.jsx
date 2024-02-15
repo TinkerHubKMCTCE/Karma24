057b74d9bd4275c2749626b15fbfd51c86f073ee
Hashimrahman <hashimrahmanrh@gmail.com>
2024-02-15 10:43:10 +0530

    Fix:Payment Pages

--- src/views/EventDetailsView.jsx
+++ src/views/EventDetailsView.jsx
@@ -32,16 +32,13 @@ const EventDetailsView = ({ type }) => {
 
   const item = list.find((e) => e.id === id);
 
-
   const handleRegisterClick = () => {
     if (item.registerLink) {
-      window.open(item.registerLink, '_blank');
+      window.open(item.registerLink, "_blank");
     } else {
-      // Handle case where register link is not available
-      console.error("Register link not available for this item.");
+      alert("Registration will start Soon!")
     }
   };
-  
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -56,12 +53,16 @@ const EventDetailsView = ({ type }) => {
         <div>
           <p className="text-2xl font-retroTeam tracking-wide leading-8">
             {item.dateTime} <br />
-            {item.description ? item.description.map((desc) => <p>{desc}</p>) : null}
+            {item.description
+              ? item.description.map((desc) => <p>{desc}</p>)
+              : null}
           </p>
           <p className="font-bold font-retroTeam text-3xl mt-4 text-karma-green">
             {item.prize}
           </p>
-          <p className="font-bold font-retroTeam text-2xl mt-4">Fee: ₹{item.fee}/-</p>
+          <p className="font-bold font-retroTeam text-2xl mt-4">
+            Fee: ₹{item.fee}/-
+          </p>
           <FilledButton text="REGISTER" onClick={handleRegisterClick} />
         </div>
         <img src={item.image} alt="" className="h-72 rounded-3xl" />
